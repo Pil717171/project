ed4b6a87a375077a133a423c5ff532b115fc80d5
Admin-PC\Admin <pil717171@gmail.com>
2018-12-11 17:31:24 +0300

    add 2 dropdownmenu

--- src/js/component/header.js
+++ src/js/component/header.js
@@ -1,7 +1,32 @@
-var dropdown = document.querySelector(".header-top_region")
+(function () {
+    var dropdown = document.querySelector(".header-top_region")
 
-function dropdownOpen () {
-    var dropdownMenu = document.querySelector(".header-top_region_dropdown_menu")
-    dropdownMenu.classList.toggle("header-top_region_dropdown_menu--open");
-}
-dropdown.onclick = dropdownOpen
+    function dropdownOpen () {
+        var dropdownMenu = document.querySelector(".header-top_region_dropdown_menu")
+        dropdownMenu.classList.toggle("header-top_region_dropdown_menu--open");
+    }
+    dropdown.onclick = dropdownOpen
+})();
+
+(function (){
+    var dropdownTire = document.querySelector(".header-bottom_menu_tires_dropdown");
+    var icon = document.querySelector(".header-bottom_menu_tires_icon");
+    var dropdownMenuBottom = document.querySelector(".header-bottom_menu_tires");
+    
+    function dropdownTires () {
+        dropdownTire.classList.add("header-bottom_menu_tires_dropdown--open");
+        icon.classList.add("header-bottom_menu_tires_icon--rotate"); 
+        dropdownMenuBottom.classList.add("header-bottom_menu_tires--shadow");               
+    }
+
+    function dropdownTiresClosed () {
+        dropdownTire.classList.remove("header-bottom_menu_tires_dropdown--open");
+        icon.classList.remove("header-bottom_menu_tires_icon--rotate");
+        dropdownMenuBottom.classList.remove("header-bottom_menu_tires--shadow");
+    }
+    dropdownMenuBottom.onmouseover = dropdownTires;
+    dropdownTire.onmouseover = dropdownTires;
+    dropdownMenuBottom.onmouseout = dropdownTiresClosed;
+    dropdownTire.onmouseout = dropdownTiresClosed;
+})()
+    
