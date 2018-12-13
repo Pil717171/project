3be54ac62a2e1c4e3c4f1d0c3d0910c076e170a6
Admin-PC\Admin <pil717171@gmail.com>
2018-12-14 00:18:47 +0300

    add component calendar

--- src/js/component/calendar.js
+++ src/js/component/calendar.js
@@ -1,54 +1,18 @@
-var widthBig = document.querySelector(".calendar-mixin").offsetWidth;
+var slider = document.querySelector(".calendar-mixin");
 var widthSlide = document.querySelector(".calendar-slider_races").offsetWidth;
-var currentPosition = 0;
 var buttonLeft = document.querySelector(".calendar-buttons_left")
 var buttonRight = document.querySelector(".calendar-buttons_right")
 
-
-console.log(widthBig)
-console.log(widthSlide.offsetLeft)
 
 buttonLeft.onclick = sliderLeft;
 buttonRight.onclick = sliderRight;
 
 function sliderLeft () {
-    currentPosition -=widthSlide;
-    console.log(currentPosition)
+    slider.style.transform += "translateX(" + widthSlide  + "px)"
+   
 }
 
 function sliderRight () {
-    currentPosition +=widthSlide;
-    console.log(currentPosition)
+    slider.style.transform += "translateX(" + -widthSlide  + "px)"
+    
 }
-
-// document.querySelectorAll('.calendar-slider').forEach(function (element) {
-//     var slideSize = element.clientWidth,
-//         currentPosition = 0,
-//         buttonLeft = document.querySelector('.calendar-buttons_left'),
-//         buttonRight = document.querySelector('.calendar-buttons_right');
-
-//         buttonLeft.onclick = sliderLeft;
-//         buttonRight.onclick = sliderRight;
-
-//         function sliderLeft () {
-//             currentPosition -= slideSize;
-//             if (currentPosition >= element.scrollWidth) {
-//             currentPosition = element.scrollWidth - 2*slideSize;
-//             }
-//             element.scroll({
-//                 left: currentPosition,
-//                 behavior: 'smooth'
-//               });
-//         };
-
-//         function sliderRight () {
-//             currentPosition += slideSize;
-//             if (currentPosition < 0 ) {
-//                 currentPosition = slideSize
-//             }
-//             element.scroll({
-//                 left: currentPosition,
-//                 behavior: 'smooth'
-//             });
-//         }
-// });
